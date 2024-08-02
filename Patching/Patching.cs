58f8187896d0b5afb61f5393677e13f7e8f5e43a
Mabel <mae@weatherelectric.xyz>
2024-08-01 20:09:11 -0700

    Improved method of adding ammo

--- Patching/Patching.cs
+++ Patching/Patching.cs
@@ -1,5 +1,7 @@
 using HarmonyLib;
 using Il2CppSLZ.Marrow;
+using Il2CppSLZ.Marrow.Data;
+// ReSharper disable InconsistentNaming
 
 namespace WeatherElectric.InfiniteAmmo.Patching;
 
@@ -11,22 +13,32 @@ public static class AmmoInventoryPatch
     public static void Awake(AmmoInventory __instance)
     {
         if (!Preferences.Enabled.Value) return;
-        ModConsole.Msg("Adding shit ton of ammo", 1);
-        __instance.AddCartridge(__instance.lightAmmoGroup, 500000);
-        __instance.AddCartridge(__instance.mediumAmmoGroup, 500000);
-        __instance.AddCartridge(__instance.heavyAmmoGroup, 500000);
+        
+        __instance.AddCartridge(__instance.lightAmmoGroup, 1000);
+        __instance.AddCartridge(__instance.mediumAmmoGroup, 1000);
+        __instance.AddCartridge(__instance.heavyAmmoGroup, 1000);
     }
 
     [HarmonyPatch(nameof(AmmoInventory.RemoveCartridge))]
     [HarmonyPostfix]
-    public static void RemoveCartridge(AmmoInventory __instance)
+    public static void RemoveCartridge(AmmoInventory __instance, CartridgeData cartridge, int count)
     {
         if (!Preferences.Enabled.Value) return;
-#if DEBUG
-        ModConsole.Msg("Adding a shit ton of ammo", 1);
-#endif
-        __instance.AddCartridge(__instance.lightAmmoGroup, 1);
-        __instance.AddCartridge(__instance.mediumAmmoGroup, 1);
-        __instance.AddCartridge(__instance.heavyAmmoGroup, 1);
+
+        var group = __instance.GetGroupByCartridge(cartridge);
+        if (group == null) return;
+
+        switch (group)
+        {
+            case "light":
+                __instance.AddCartridge(__instance.lightAmmoGroup, count);
+                break;
+            case "medium":
+                __instance.AddCartridge(__instance.mediumAmmoGroup, count);
+                break;
+            case "heavy":
+                __instance.AddCartridge(__instance.heavyAmmoGroup, count);
+                break;
+        }
     }
 }
